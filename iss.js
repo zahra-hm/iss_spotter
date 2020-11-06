4ca2b2cb1245e419e3c2e0ce75bea7be606bbd3b
zahra-hm <zahra520@hotmail.com>
2020-11-06 05:50:24 +0000

    fetch the ip adress and check to see if it's correct

---
+++ iss.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
